6379275d6f0a09486dabc561fa7cf0e2546ace69
duner <asduner@gmail.com>
2016-11-14 14:26:51 -0600

    Adding some initial files

---
+++ tasks/compile-hbs.js
@@ -0,0 +1,60 @@
+var _ = require('lodash'),
+    fm = require('front-matter'),
+    yml = require('yamljs'),
+    fs = require('fs-extra'),
+    path = require('path'),
+    Handlebars = require('handlebars'),
+    globby = require('globby'),
+    debug = require('./helpers/debug-hbs.js'),
+    helper = require('./helpers/partial-builder.js');
+    copy = require('./helpers/update-copy.js');
+
+data = getData('src/data/data.yml');
+copy = copy.updateCopy();
+
+//run Helpers
+helper.getPartial(Handlebars, 'src/templates/partials/');
+
+function getData(file) {
+    fileName = path.basename(file, '.yml');
+
+    return yml.load(file);
+}
+
+function renderTemplate(templatePath, data) {
+  var file = fs.readFileSync(templatePath, 'utf8'),
+      frontMatter = fm(file),
+      fmData = frontMatter.attributes,
+      context = _.extend(fmData, data),
+      template = Handlebars.compile(frontMatter.body);
+
+  return template(context);
+}
+
+function renderPage(template, layout, data) {
+  var file = fs.readFileSync(layout, 'utf8'),
+      page = Handlebars.compile(file),
+      context = _.extend({body: template}, data);
+
+  return page(context);
+}
+
+function build() {
+  var hbsTemplates = globby.sync('src/templates/**/*.hbs');
+
+  _.forEach(hbsTemplates, function(file, i) {
+    var filePattern = path.dirname(file).split('src/templates/')[1],
+        fileName = path.basename(file, '.hbs'),
+        template = renderTemplate(file, data),
+        page = renderPage(template, 'src/templates/layouts/default.hbs', data);
+
+    if(!!filePattern) {
+      fs.outputFileSync(`dist/${filePattern}/${fileName}.html`)
+    } else {
+      fs.outputFileSync(`dist/${fileName}.html`, page, 'utf8');
+    }
+  });
+
+}
+
+build();
